0c7f102648f50cf2ae57105fe5d42d0cf9c02c9f
F1lthyAn1mal <ads@adammo.co.uk>
2026-02-06 15:42:06 +0000

    finished unit tests

--- index.js
+++ index.js
@@ -11,3 +11,107 @@
 'use strict';
 
 // add your functions here
+
+function replaceText(elem, str) {
+    elem.textContent = str;
+}
+
+function addTextTo(elem, str) {
+    elem.textContent += str;
+}
+
+function moreBears() {
+    const animals = document.querySelector('#animals');
+    animals.src = 'http://placebear.com/400/200';
+    animals.alt = 'A bear.';
+    animals.title = 'A BEAR!';
+}
+
+function setId(elem, str) {
+    elem.id = str;
+    return elem;
+}
+
+function setClass(elem, str) {
+    elem.className = str;
+    return elem;
+}
+
+function addAClass(elem, str) {
+    elem.classList.add(str);
+    return elem;
+}
+
+function removeAClass(elem, str) {
+    elem.classList.remove(str);
+    return elem;
+}
+
+function newElement(name) {
+    return document.createElement(name);
+}
+
+function findElementById(id) {
+    return document.querySelector(`#${id}`);
+}
+
+function findElementsByQuery(query) {
+    return document.querySelectorAll(query);
+}
+
+function reverseList(query) {
+    const list = document.querySelector(query);
+    if (list) {
+        const items = Array.from(list.children);
+        items.reverse().forEach(item => list.appendChild(item));
+    }
+    return list;
+}
+
+function mover(moveThis, appendToThis) {
+    const elem = document.querySelector(moveThis);
+    const dest = document.querySelector(appendToThis);
+
+    dest.append(elem);
+}
+
+function filler(list, candidates) {
+    // candidates.forEach(str => {
+    //     const li = document.createElement('li');
+    //     li.textContent = str;
+    //     list.append(li);
+    // });
+    for (let item of candidates) {
+        const li = document.createElement('li');
+        li.textContent = item;
+        list.append(li);
+    }
+}
+
+function dupe(selector) {
+    const elem = document.querySelector(selector);
+    const parent = elem.parentElement;
+    const elem2 = elem.cloneNode(true);
+    parent.append(elem2);
+}
+
+function removeAll(selector) {
+    // const elems = document.querySelectorAll(selector);
+    // elems.forEach((_, i) => elems[i].remove());
+    // // for (const elem of elems) {
+    // //     elem.remove();
+    // // }
+
+    document.querySelectorAll(selector).forEach(
+        elem => elem.remove()
+    );
+}
+
+function getUserData() {
+    const user = {
+        name: document.querySelector('#username').value,
+        speed: Number(document.querySelector('#speed').value),
+        student: document.querySelector('#student').checked
+    };
+    return user;
+}
